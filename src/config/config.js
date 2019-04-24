97cf7d6a9f7809ee0d44294a2086e16eae3c6fad
Jhon Velasquez <jhonvelasquezudea@gmail.com>
2019-04-24 17:05:23 -0500

    elimino clave sengrid api

--- src/config/config.js
+++ src/config/config.js
@@ -4,5 +4,4 @@ if (!process.env.URLDB) {
     process.env.URLDB = "mongodb://localhost:27017/Education";
 }
 
-process.env.SENDGRID_API_KEY =
-    "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";
+process.env.SENDGRID_API_KEY = "";
