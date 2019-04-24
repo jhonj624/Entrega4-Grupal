d8b6aa5577b117d1f219de7ed05ea4b0b6da2f37
Jhon Velasquez <jhonvelasquezudea@gmail.com>
2019-04-24 16:35:30 -0500

    Se desarrolla la HU3, se notifica al docente del curso cerrado y fecha de inicio

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,8 @@
-process.env.PORT = process.env.PORT || 3000;
-
+process.env.PORT = process.env.PORT || 3002;
 
 if (!process.env.URLDB) {
-
-    process.env.URLDB = 'mongodb://localhost:27017/Education'
-
+    process.env.URLDB = "mongodb://localhost:27017/Education";
 }
+
+process.env.SENDGRID_API_KEY =
+    "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";
