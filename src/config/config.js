cd7762a0436f0dd0e09abd819d8ccacd799564bd
Jhon Velasquez <jhonvelasquezudea@gmail.com>
2019-04-24 23:36:25 -0500

    se configura entorno de trabajo env

--- src/config/config.js
+++ src/config/config.js
@@ -1,13 +1,18 @@
 process.env.PORT = process.env.PORT || 3000;
 
+// Entorno
+process.env.NODE_ENV = process.env.NODE_ENV || "dev"; // bien
+
 let urlDB;
-if (process.env.NODE_ENV === "local") {
+if (process.env.NODE_ENV === "dev") {
     urlDB = "mongodb://localhost:27017/Education";
 } else {
     urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
-
+console.log(urlDB);
 /* process.env.SENDGRID_API_KEY =
     "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"; */
+process.env.SENDGRID_API_KEY = process.env.SG_KEY;
+console.log("sendgrid key:" + process.env.SENDGRID_API_KEY);
