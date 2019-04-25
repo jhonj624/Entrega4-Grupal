3479a33e37f6377b9ce3d11ec61f771ef181c5c0
Jhon Velasquez <jhonvelasquezudea@gmail.com>
2019-04-24 23:12:50 -0500

    Se agrega el acceso a mlab: mongo en la web

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,13 @@
 process.env.PORT = process.env.PORT || 3000;
 
-if (!process.env.URLDB) {
-    process.env.URLDB = "mongodb://localhost:27017/Education";
+let urlDB;
+if (process.env.NODE_ENV === "local") {
+    urlDB = "mongodb://localhost:27017/Education";
+} else {
+    urlDB = process.env.MONGO_URI;
 }
+
+process.env.URLDB = urlDB;
 
 /* process.env.SENDGRID_API_KEY =
     "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"; */
